20a2d74e1573f07130b6d964e3f6c321828a90e6
Damian Sikora <dsikora85@gmail.com>
2020-11-08 20:25:47 +0100

    Icons and app name

--- BusSchedule.Android/MainActivity.cs
+++ BusSchedule.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace BusSchedule.Droid
 {
-    [Activity(Label = "BusSchedule", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "Rozkład jazdy MZK Wejherowo", Icon = "@mipmap/icon", RoundIcon = "@mipmap/icon_round", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
