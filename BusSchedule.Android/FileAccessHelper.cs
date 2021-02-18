d9a8cf3654e48b60c9af80b136ddb781c0e57db2
Damian Sikora <dsikora85@gmail.com>
2021-02-18 20:36:33 +0100

    Catch copy file errors

--- BusSchedule.Android/FileAccessHelper.cs
+++ BusSchedule.Android/FileAccessHelper.cs
@@ -1,4 +1,5 @@
 ﻿using BusSchedule.Interfaces;
+using System;
 using System.IO;
 using System.Text;
 using System.Threading.Tasks;
@@ -33,19 +34,29 @@ namespace BusSchedule.Droid
             var tcs = new TaskCompletionSource<bool>(TaskCreationOptions.RunContinuationsAsynchronously);
             Task.Run(() =>
             {
-                if(File.Exists(destFilename))
+                try
                 {
-                    File.Delete(destFilename);
+                    if (File.Exists(destFilename))
+                    {
+                        File.Delete(destFilename);
+                    }
+                    using var br = new BinaryReader(Android.App.Application.Context.Assets.Open(assetFilename));
+                    using var bw = new BinaryWriter(new FileStream(destFilename, FileMode.Create));
+                    byte[] buffer = new byte[2048];
+                    int length = 0;
+                    while ((length = br.Read(buffer, 0, buffer.Length)) > 0)
+                    {
+                        bw.Write(buffer, 0, length);
+                    }
                 }
-                using var br = new BinaryReader(Android.App.Application.Context.Assets.Open(assetFilename));
-                using var bw = new BinaryWriter(new FileStream(destFilename, FileMode.Create));
-                byte[] buffer = new byte[2048];
-                int length = 0;
-                while ((length = br.Read(buffer, 0, buffer.Length)) > 0)
+                catch(Exception exc)
                 {
-                    bw.Write(buffer, 0, length);
+                    var msg = exc.Message;
                 }
-                tcs.SetResult(true);
+                finally
+                {
+                    tcs.SetResult(true);
+                }
             }).ConfigureAwait(false);
 
 			return tcs.Task;
