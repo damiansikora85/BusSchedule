b0936c6b7db4683cc103360462265fb6190142a2
Damian Sikora <dsikora85@gmail.com>
2021-06-06 19:47:54 +0200

    Updated schedule database

--- BusSchedule/App.xaml.cs
+++ BusSchedule/App.xaml.cs
@@ -13,7 +13,7 @@ namespace BusSchedule
 {
     public partial class App : Application
     {
-        public static string DB_FILENAME = "sqlite20210518.db";
+        public static string DB_FILENAME = "sqlite20210603.db";
         public App()
         {
             InitializeComponent();
