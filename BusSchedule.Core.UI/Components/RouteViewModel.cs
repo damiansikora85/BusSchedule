c777667e81bff291ad89e819486f171b536a1a66
Damian Sikora <dsikora85@gmail.com>
2021-09-12 21:03:53 +0200

    Use colors for routes

--- BusSchedule.Core.UI/Components/RouteViewModel.cs
+++ BusSchedule.Core.UI/Components/RouteViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using BusSchedule.Core.Model;
 using System;
+using System.Drawing;
 using System.Windows.Input;
 
 namespace BusSchedule.Components.ViewModels
@@ -9,10 +10,27 @@ namespace BusSchedule.Components.ViewModels
         public string Name => Route.Route_Short_Name;
         public Action<Routes> OnClick;
         public Routes Route { get; }
+        public Color RouteColor { get; }
+        public Color RouteTextColor { get; }
 
         public RouteViewModel(Routes busService)
         {
             Route = busService;
+            RouteColor = GetColorFromString(Route.Route_Color);
+            RouteTextColor = GetColorFromString(Route.Route_Text_Color);
+        }
+
+        private Color GetColorFromString(string colorString)
+        {
+            var red = StringToHex(colorString.Substring(0, 2));
+            var green = StringToHex(colorString.Substring(2, 2));
+            var blue = StringToHex(colorString.Substring(4, 2));
+            return Color.FromArgb(red, green, blue);
+        }
+
+        private int StringToHex(string hex)
+        {
+            return int.Parse(hex, System.Globalization.NumberStyles.HexNumber);
         }
     }
 }
