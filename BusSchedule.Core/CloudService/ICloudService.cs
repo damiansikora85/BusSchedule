92629d39b6c5d14731f168ab7e19d1a6b9dfc4f6
Damian Sikora <dsikora85@gmail.com>
2021-09-18 14:33:42 +0200

    Firebase functions

---
+++ BusSchedule.Core/CloudService/ICloudService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusSchedule.Core.CloudService
+{
+    public interface ICloudService
+    {
+        Task<string> TestGet();
+        Task<string> GetLatestScheduleFilename();
+    }
+}
