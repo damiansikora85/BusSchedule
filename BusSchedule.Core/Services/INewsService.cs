7765f97cc641edbccad21519610eb24be713e0f6
Damian Sikora <dsikora85@gmail.com>
2021-12-19 18:57:42 +0100

    News service

---
+++ BusSchedule.Core/Services/INewsService.cs
@@ -0,0 +1,17 @@
+﻿using BusSchedule.Core.Model;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System;
+
+namespace BusSchedule.Core.Services
+{
+    public interface INewsService
+    {
+        public event EventHandler NewsUpdated;
+        public int NewsCount { get; }
+
+        public Task UpdateNews();
+
+        public IList<News> GetNews();
+    }
+}
---
+++ BusSchedule.Core/Services/NewsService.cs
@@ -0,0 +1,34 @@
+﻿using BusSchedule.Core.CloudService;
+using BusSchedule.Core.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BusSchedule.Core.Services
+{
+    public class NewsService : INewsService
+    {
+        private ICloudService _cloudService;
+        public event EventHandler NewsUpdated;
+        public int NewsCount => _news.Count;
+        private IList<News> _news;
+
+        public NewsService(ICloudService cloudService)
+        {
+            _cloudService = cloudService;
+            _news = new List<News>();
+        }
+
+        public async Task UpdateNews()
+        {
+            _news = await _cloudService.GetNews();
+            NewsUpdated?.Invoke(this, EventArgs.Empty);
+        }
+
+        public IList<News> GetNews()
+        {
+            return Enumerable.Empty<News>().ToList();
+        }
+    }
+}
