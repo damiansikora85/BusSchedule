16602895d96fea9b4a9ca41f08088c56b0057b0c
Damian Sikora <dsikora85@gmail.com>
2022-01-20 21:03:31 +0100

    Show correct schedule on public holidays

---
+++ BusSchedule.Core.UI/Utils/HolidaysHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusSchedule.Core.UI.Utils
+{
+    public static class HolidaysHelper
+    {
+        private static List<string> PublicHolidays = new List<string>
+        {
+            "01.01" ,
+            "06.01",
+            "01.05",
+            "03.05",
+            "15.08",
+            "01.11",
+            "11.11",
+            "25.12",
+            "26.12"
+        };
+
+        public static bool IsTodayHoliday()
+        {
+            return PublicHolidays.Contains(DateTime.Now.ToString("dd.MM"));
+        }
+    }
+}
