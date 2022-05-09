a773870ddd43d1aa894764eac86aace9464c1746
Damian Sikora <dsikora85@gmail.com>
2022-05-09 20:46:05 +0200

    Fixed schedule

--- BusSchedule/App.xaml.cs
+++ BusSchedule/App.xaml.cs
@@ -23,7 +23,7 @@ namespace BusSchedule
 {
     public partial class App : Application
     {
-        public static string DB_FILENAME = "sqlite20220401.db";
+        public static string DB_FILENAME = "sqlite20220508.db";
         public App()
         {
             InitializeComponent();
