58f3e30bd46845eb2eb8ddf1df0e665b9230a059
Damian Sikora <dsikora85@gmail.com>
2023-11-12 21:38:22 +0100

    Fixed displaying services and popup

--- BusSchedule/Pages/RoutesPage.xaml.cs
+++ BusSchedule/Pages/RoutesPage.xaml.cs
@@ -107,7 +107,6 @@ public partial class RoutesPage : ContentPage
                 var dialog = new RouteSelectionDialog(destination);
                 await this.ShowPopupAsync(dialog);
                 var selectedDirection = await dialog.WaitForResult();
-                //await Navigation.PushAsync(new RoutePage(route, selectedDirection == 0 ? destination.Outbound : destination.Inbound, selectedDirection));
                 await Navigation.PushAsync(new RoutePage(route, selectedDirection == 0 ? destination.Outbound : destination.Inbound, selectedDirection));
             }
             else if (!string.IsNullOrEmpty(destination.Outbound) || !string.IsNullOrEmpty(destination.Inbound))
