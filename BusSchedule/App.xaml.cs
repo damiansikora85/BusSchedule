d7a48a4e3e8dda05b8d084f72d99c02d211c3f9d
Damian Sikora <dsikora85@gmail.com>
2024-10-04 21:35:17 +0200

    Updated schedule

--- BusSchedule/App.xaml.cs
+++ BusSchedule/App.xaml.cs
@@ -18,7 +18,7 @@ namespace BusSchedule
 {
     public partial class App : Application
     {
-        public static string DB_FILENAME = "sqlite20240715.db";
+        public static string DB_FILENAME = "sqlite20241005.db";
         private SemaphoreSlim _updateSemafor = new SemaphoreSlim(1);
         public App()
         {
