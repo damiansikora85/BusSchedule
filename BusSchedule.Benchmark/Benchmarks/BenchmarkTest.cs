ff6ead95a5d3469cfb33f5d80f91bacec267e19c
Damian Sikora <dsikora85@gmail.com>
2026-01-07 21:14:08 +0100

    New benchmark proj

---
+++ BusSchedule.Benchmark/Benchmarks/BenchmarkTest.cs
@@ -0,0 +1,19 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace BusSchedule.Benchmark
+{
+    public class BenchmarkTest
+    {
+        // And define a method with the Benchmark attribute
+        [Benchmark]
+        public void Sleep() => Thread.Sleep(10);
+
+        // You can write a description for your method.
+        [Benchmark(Description = "Thread.Sleep(10)")]
+        public void SleepWithDescription() => Thread.Sleep(10);
+    }
+}
---
+++ BusSchedule.Benchmark/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace BusSchedule.Benchmark
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
