5876c8135326190c1e2224030315e690ca926cb4
Damian Sikora <dsikora85@gmail.com>
2026-08-25 21:21:21 +0200

    Updated schedule

--- BusScheduleMaui/App.xaml.cs
+++ BusScheduleMaui/App.xaml.cs
@@ -15,7 +15,7 @@ namespace BusSchedule;
 
 public partial class App : Application
 {
-    public static string DB_FILENAME = "sqlite20260629.db";
+    public static string DB_FILENAME = "sqlite20260825.db";
     private SemaphoreSlim _updateSemafor = new SemaphoreSlim(1);
     private CancellationTokenSource _updateCancellationTokenSource = new CancellationTokenSource();
     public App()
